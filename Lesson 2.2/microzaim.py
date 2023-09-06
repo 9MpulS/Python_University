2172dcb114c977931e18daacebd86653e7f63788
Vlad <95881305+9MpulS@users.noreply.github.com>
2023-09-06 23:27:00 +0300

    Delete Lesson 2.2 directory

--- Lesson 2.2/microzaim.py
+++
@@ -1,8 +0,0 @@
-# код в модулі
-def microzaim(stipend, fst_expenses):   # ф-я для розрахунку суми грошей, яку необхідно попросити в батьків
-    lst_expenses = fst_expenses
-    total = 0
-    for i in range(10):
-        total += lst_expenses
-        lst_expenses *= 1.05
-    return total - (stipend*10)  # витрати за рік мінус стипендія за рік
